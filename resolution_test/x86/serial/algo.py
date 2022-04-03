e12d9eb4d9862bdee5d8592b0cb4faaadf7131a2
Sicheng Zuo <479726995@qq.com>
2022-04-03 18:54:10 +0800

    给脚本添加了注释，并进行了优化

--- resolution_test/x86/serial/algo.py
+++ resolution_test/x86/serial/algo.py
@@ -3,7 +3,9 @@ import sys
 import getopt
 import numpy as np
 import pandas as pd
+import multiprocessing
 from sklearn import cluster
+from functools import partial
 from collections import Counter
 from sklearn.ensemble import IsolationForest
 from scipy.stats import wasserstein_distance
@@ -12,11 +14,13 @@ from scipy.stats import wasserstein_distance
 # frequency = 2.6
 confidence_level = 0.05
 data_level = 1 - confidence_level
+ncpus = multiprocessing.cpu_count()
 constant_rates = {"cas114": 2494103, "cas113": 249414}
-ins_lat = {"ADD": 1, "MUL": 3, "FMA": 4, "LD": 0.5, "ST": 1}
+# ins_lat = {"ADD": 1, "MUL": 3, "FMA": 4, "LD": 0.5, "ST": 1}
 
 
 def usage():
+    ''' Print some info about how to use this script. '''
     print("This script is used to process the result csv files")
     print("-h or --help     print this message")
     print("-s or --start    <start gid>,<start pid>")
@@ -25,19 +29,30 @@ def usage():
     print("-b or --backend  <pfhd or papi>")
     print("-f or --function <apply data process functions>")
     print("-a or --fargs <arguments of functions, split with comma>")
-    print("if you choose timing_cost, no fargs are needed")
-    print("if you choose least_interval, the fargs are <start_round,cost_cycle,cost_nanosec>")
-    print("if you choose variation_resolution, the fargs are <least_round,cost_cycle,cost_nanosec>")
+    print("if you choose cost_measure, the farg <result file of cost_measure> is needed")
+    print("if you choose least_interval, the fargs are <current test round>,\
+          <result file of cost_measure>, <result file of least_interval>")
+    print("if you choose variation_resolution, the fargs are <current delta round>,\
+          <least measurable round>, <result file of cost_measure>, <result file of variation_resolution>")
 
 
 def parse_option():
+    ''' Parse the command line options
+
+        Parse the command line options that are needed to run this script.
+        The details of the command line options are shown in the content of 'usage' function.
+
+        Returns:
+            options_dict: A dict that contains key-value pairs of command line options
+    '''
     try:
+        # parse the command line options
         opts, args = getopt.getopt(sys.argv[1:], "hs:e:i:b:f:a:", ["start=", "end=", "input=", "backend=", "function=", "args="])
     except getopt.GetoptError:
         print("parameter format error")
         usage()
         sys.exit(-1)
-
+    # store the parsed command line options in the dict
     options_dict = dict()
     for opt, arg in opts:
         if opt in ('-h', '--help'):
@@ -74,6 +89,17 @@ def parse_option():
 
 
 def parse_rankmap(options_dict):
+    ''' Parse the file rankmap.csv
+
+        Parse the rankmap.csv and get the info of rank id, cpu id and hostname.
+        Then, create the output directory to store result data.
+
+        Args:
+            option_dict: the dict of parsed commandline options.
+
+        Returns:
+            rankmap_dict: A dict that contains the value of rank id, cpu id and hostname.
+    '''
     input_dir = options_dict["input"]
     rankmap_path = f"{input_dir}/rankmap.csv"
     rankmap_df = pd.read_csv(rankmap_path)
@@ -81,12 +107,25 @@ def parse_rankmap(options_dict):
     for key in ["rank", "cpu", "hostname"]:
         rankmap_dict[key] = rankmap_df[key].values[0]
     dir_path = f"{options_dict['output']}/{rankmap_dict['hostname']}"
+    # create the output directory
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     return rankmap_dict
 
 
 def interval2csv(options_dict, rankmap_dict):
+    ''' Process the original data
+
+        Process the original data according to the command line options,
+        and write the result data to the output directory.
+
+        Args:
+            options_dict: the dict of parsed commandline options.
+            rankmap_dict: the dict of parsed rankmap.csv file.
+
+        Returns:
+            res_df: the pandas.DataFrame of the result data
+    '''
     sgid = options_dict["start_gid"]
     spid = options_dict["start_pid"]
     egid = options_dict["end_gid"]
@@ -97,6 +136,7 @@ def interval2csv(options_dict, rankmap_dict):
     src_file = f"{options_dict['input']}/{hostname}/r{rank_id}c{cpu_id}.csv"
     dst_file = f"{options_dict['output']}/{hostname}/r{rank_id}c{cpu_id}.csv"
     host_prefix = hostname.split(".")[0]
+    # read the original data
     src_df = pd.read_csv(src_file)
     col_names = list(src_df.columns.values)
     col_names.remove("gid")
@@ -105,20 +145,41 @@ def interval2csv(options_dict, rankmap_dict):
     src_df_ep = src_df[(src_df["gid"] == egid) & (src_df["pid"] == epid)][col_names]
     src_df_sp.reset_index(inplace=True)
     src_df_ep.reset_index(inplace=True)
+    # use the data of (end_gid, end_pid) to minus the data of (start_gid, start_pid)
     res_df = (src_df_ep - src_df_sp)[col_names]
     if options_dict["backend"] == "pfhd":
         res_df["nanosec"] = res_df["nanosec"].apply(lambda x: int(x * 1000000 // constant_rates[host_prefix]))
+    # store the result data
     res_df.to_csv(dst_file, index=False)
     return res_df
 
 
 def get_res_df(options_dict):
+    ''' Wrapper function of "parse_rankmap" and "interval2csv"
+
+        Wrapper function of "parse_rankmap" and "interval2csv",
+        used to get the dataframe of result data
+
+        Args:
+            options_dict: the dict of parsed commandline options.
+
+        Returns:
+            res_df: the pandas.DataFrame of the result data
+    '''
     rankmap_dict = parse_rankmap(options_dict)
     res_df = interval2csv(options_dict, rankmap_dict)
     return res_df
 
 
-def timing_cost(options_dict):
+def cost_measure(options_dict):
+    ''' Get the cost of measure function
+
+        Get the cost of measure function and write the result to the output file.
+        The path of output file is specified by command line options.
+
+        Args:
+            options_dict: the dict of parsed commandline options.
+    '''
     cost_res_file = options_dict["fargs"][0]
     res_df = get_res_df(options_dict)
     cost_nanosec = res_df["nanosec"].min()
@@ -127,33 +188,71 @@ def timing_cost(options_dict):
         fp.write(f"{cost_cycle}\n{cost_nanosec}\n")
 
 
-def get_best_param(data, start, factor):
-    density = list()
-    params = [(x / factor) + start for x in range(11)]
-    for param in params:
-        if param == 0:
-            org_cycle_width = np.max(data[:, 0]) - np.min(data[:, 0])
-            org_nanosec_width = np.max(data[:, 1]) - np.min(data[:, 1])
-            density.append(data.shape[0] / (org_cycle_width * org_nanosec_width))
-        else:
-            labels = IsolationForest(random_state=42, contamination=param).fit_predict(data)
-            target_idx = np.array([(label == 1) for label in labels])
-            new_data = data[target_idx]
-            cycle_width = np.max(new_data[:, 0]) - np.min(new_data[:, 0])
-            cycle_nanosec = np.max(new_data[:, 1]) - np.min(new_data[:, 1])
-            density.append(new_data.shape[0] / (cycle_width * cycle_nanosec))
-    return params[np.argmax(np.diff(density))]
+def IsolationForest_filter(param, data):
+    ''' The help function of "rm_noise"
+
+        Given the original data and a parameter, use the parameter to run
+        IsolationForest and get the data after filtering out noise points.
+        Also, we will calculate the point density corresponding to this parameter.
+        The point density is (the number of filtered points) / ((max_cycle - min_cycle)*(max_ns - max_ns)).
+
+        Args:
+            param: the value for the contamination parameter of IsolationForest
+            data: the original data
+
+        Returns:
+            density: the point density corresponding to the parameter
+    '''
+    if param == 0:
+        org_cycle_width = np.max(data[:, 0]) - np.min(data[:, 0])
+        org_nanosec_width = np.max(data[:, 1]) - np.min(data[:, 1])
+        density = (data.shape[0] / (org_cycle_width * org_nanosec_width))
+    else:
+        labels = IsolationForest(random_state=42, contamination=param).fit_predict(data)
+        target_idx = np.array([(label == 1) for label in labels])
+        new_data = data[target_idx]
+        cycle_width = np.max(new_data[:, 0]) - np.min(new_data[:, 0])
+        cycle_nanosec = np.max(new_data[:, 1]) - np.min(new_data[:, 1])
+        density = (new_data.shape[0] / (cycle_width * cycle_nanosec))
+    return density
 
 
 def rm_noise(org_df):
+    ''' The function to remove the noise points in the original data
+
+        Use ten-point search method (similar to a binary search) to search
+        for the best parameters of IsolationForest. For example, in the first round,
+        we iterate over the parameters [0, 0.01, 0.02, ..., 0.09, 0.1] in steps of 0.01,
+        and in the second round, we traverse the parameters [0.03, 0.031, ..., 0.039, 0.4]
+        in the steps of 0.001. The step size is controlled by the variable "factor". For
+        each parameter, we calculate its corresponding point density (see the description in
+        function `IsolationForest_filter`). The best parameter of each round appears where
+        there is a sudden change in the point density.
+
+        After finding the best parameter, we then use the IsolationForest to filter the noise
+        points in the original data.
+
+        Args:
+            org_df: the dataframe contains the orginal data.
+
+        Returns:
+            new_df: the dataframe contains data after filtering out noise points.
+    '''
     start = 0
     factor = 100
     data_cycle = np.array(org_df["cycle"])
     data_nanosec = np.array(org_df["nanosec"])
     data = np.array([data_cycle, data_nanosec]).T
-    while factor <= 1000:
-        start = get_best_param(data, start, factor)
-        factor *= 10
+    partial_func = partial(IsolationForest_filter, data=data)
+    # use multi process to accelerate
+    with multiprocessing.Pool(processes=ncpus) as pool:
+        # use ten-point search method to search the best parameters
+        while factor <= 1000:
+            filter_results = list()
+            params = [(x / factor) + start for x in range(11)]
+            density_results = pool.map(partial_func, params)
+            start = params[np.argmax(np.diff(density_results))]
+            factor *= 10
     best_param = (start + (10 / factor))
     labels = IsolationForest(random_state=42, contamination=best_param).fit_predict(data)
     tmp_idx = np.array([(label == 1) for label in labels])
@@ -161,6 +260,7 @@ def rm_noise(org_df):
     tmp_max_cycle = np.max(tmp_data[:, 0])
     tmp_max_nanosec = np.max(tmp_data[:, 1])
     noise_idx = [i for i in range(len(labels)) if (labels[i] != 1)]
+    # classify some eligible noise points as normal points
     for idx in noise_idx:
         if (data[idx, 0] <= tmp_max_cycle) and (data[idx, 1] <= tmp_max_nanosec):
             tmp_idx[idx] = True
@@ -169,13 +269,27 @@ def rm_noise(org_df):
 
 
 def least_interval(options_dict):
+    ''' The function to find the least measurable interval
+
+        The least measurable interval means that the fluctuations introduced by
+        the measurement itself are kept below a certain range. Take the cycle data
+        as an example, take the confidence level as 0.05, as long as
+        (np.quantile(cycle_data, 0.95) - np.min(cycle_data)) / np.min(cycle_data) < 0.05,
+        the condition can be considered satisfied. The final result will be written to the
+        output file. The path of output file is specified by command line options.
+
+        Args:
+            options_dict: the dict of parsed commandline options.
+    '''
     test_round = int(options_dict["fargs"][0])
     cost_res_file = options_dict["fargs"][1]
     interval_res_file = options_dict["fargs"][2]
+    # get the measure cost
     with open(cost_res_file, 'r') as fp:
         lines = fp.readlines()
         cost_cycle = int(lines[0].strip())
         cost_nanosec = int(lines[1].strip())
+    # get the data after filtering out noise points
     new_res_df = rm_noise(get_res_df(options_dict))
     all_cycles = np.array(new_res_df['cycle'], dtype=np.int64) - cost_cycle
     all_nanosecs = np.array(new_res_df['nanosec'], dtype=np.int64) - cost_nanosec
@@ -187,6 +301,7 @@ def least_interval(options_dict):
     percent_nanosec = np.quantile(all_nanosecs, data_level)
     interval_cycle = percent_cycle - min_cycle
     interval_nanosec = percent_nanosec - min_nanosec
+    # judge whether the conditions are satisfied
     cond_1 = (((interval_cycle / min_cycle)) <= confidence_level)
     cond_2 = (((interval_nanosec / min_nanosec)) <= confidence_level)
     with open(interval_res_file, 'w') as fp:
@@ -219,18 +334,47 @@ def least_interval(options_dict):
 
 
 def variation_judge(round_1_df, round_2_df, cost, indicator):
+    ''' The help function of "variation resolution"
+
+        Given two measurement data, determine whether they are distinguishable,
+        and calculate the wasserstein distance of the two data distributions.
+
+        Args:
+            round_1_df: the dataframe of the first measurement data
+            round_2_df: the dataframe of the second measurement data
+            cost: the cost of measurement
+            indicator: Controls whether to use cycle data or nanosec data
+
+        Returns:
+            cond: whether the two measurement data are distinguishable
+            dist_w: the wasserstein distance of the two data distributions
+    '''
     round_1_data = np.array(round_1_df[indicator], dtype=np.int64) - cost
     round_2_data = np.array(round_2_df[indicator], dtype=np.int64) - cost
+    # the condition that the two data can be distinguished
     cond = (np.min(round_2_data) > np.quantile(round_1_data, 1 - confidence_level))
     dist_w = wasserstein_distance(round_1_data, round_2_data)
     return cond, dist_w
 
 
 def variation_resolution(options_dict):
+    ''' The function to get the resolution
+
+        Given the least measurable round (r) and the delta round (k),
+        this function will process the data of round [r, r + k, r + 2k, ..., r + 20k],
+        and then check whether the data of round r + i * k and round r + (i + 1) * k is
+        distinguishable (i = 0, 1, 19). If this condition is satisfied, the result of
+        resolution will be written to the output file. The path of output file is
+        specified by command line options.
+
+        Args:
+            options_dict: the dict of parsed commandline options.
+    '''
     k = int(options_dict["fargs"][0])
     least_round = int(options_dict["fargs"][1])
     cost_res_file = options_dict["fargs"][2]
     resolution_res_file = options_dict["fargs"][3]
+    # get the measure cost
     with open(cost_res_file, 'r') as fp:
         lines = fp.readlines()
         cost_cycle = int(lines[0].strip())
@@ -262,16 +406,17 @@ def variation_resolution(options_dict):
         if np.sum(cond_res) == test_num:
             indicator_resolution = int(np.max(dist_w_res))
             fp.write(f"yes\n{indicator_resolution}\n")
-            print(f"resolution {k} round passed, {indicator_resolution} {indicator}")
+            print(f"resolution {k} round passed, {indicator_resolution} {indicator}s")
         else:
             fp.write("no\n")
             print(f"resolution {k} round failed, {np.sum(cond_res)} of {test_num}")
 
 
 def main():
+    ''' The main function '''
     options_dict = parse_option()
-    if options_dict["function"] == "timing_cost":
-        timing_cost(options_dict)
+    if options_dict["function"] == "cost_measure":
+        cost_measure(options_dict)
     elif options_dict["function"] == "least_interval":
         least_interval(options_dict)
     elif options_dict["function"] == "variation_resolution":
--- resolution_test/x86/serial/main.sh
+++ resolution_test/x86/serial/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# some variables
+# variables for test
 mode=TS
 kernel=ADD
 bind_core=7
@@ -8,13 +8,16 @@ tool=PAPI
 arrlen=720896
 measure_num=10000
 src_file=test_x86_serial.c
+
+# variables for data process script
 output_dir=""
-timing_cost_file="cost.txt"
+cost_measure_file="cost.txt"
 least_interval_file="interval.txt"
 cy_resolution_file="cycle_resolution.txt"
 ns_resolution_file="nanosec_resolution.txt"
 resolution_test_num=20
 
+# variables to store result
 cost_cycle=0
 cost_nanosec=0
 least_round=0
@@ -33,7 +36,12 @@ fi
 
 function compile_and_run() {
     round=${1}
-    output_dir="./data/${backend}/arrlen_${arrlen}/${mode}_${kernel}_test_6248_20220222/round_${round}/run_1"
+    if [ ! ${2} ]; then
+        run_id="1"
+    else
+        run_id=${2}
+    fi
+    output_dir="./data/${backend}/arrlen_${arrlen}/${mode}_${kernel}_test_6248_20220222/round_${round}/run_${run_id}"
     if [ ! -d ${output_dir} ]; then
         exe=./${tool}_${mode}_${kernel}_${round}.x
         flags="-O3 -std=gnu99 -D$tool -DMODE=$mode -DKNAME=$kernel -DROUND=$round -DARRLEN=$arrlen -DNMEASURE=$measure_num"
@@ -44,21 +52,20 @@ function compile_and_run() {
     fi
 }
 
-function timing_cost_test() {
+function cost_measure_test() {
     compile_and_run 0
     # get the cost of timing instruction
-    python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f timing_cost -a ${timing_cost_file}
-    cost_cycle=`sed -n '1p' ${timing_cost_file}`
-    cost_nanosec=`sed -n '2p' ${timing_cost_file}`
+    python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f cost_measure -a ${cost_measure_file}
+    cost_cycle=`sed -n '1p' ${cost_measure_file}`
+    cost_nanosec=`sed -n '2p' ${cost_measure_file}`
 }
 
 function interval_check() {
     target_round=${1}
-    for j in `seq 0 9`
+    for j in `seq 2 11`
     do
-        rm -rf ${output_dir}
-        compile_and_run $target_round
-        python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f least_interval -a ${target_round},${timing_cost_file},${least_interval_file}
+        compile_and_run $target_round $j
+        python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f least_interval -a ${target_round},${cost_measure_file},${least_interval_file}
         passed=`cat ${least_interval_file}`
         if [[ ${passed} == "no" ]]; then
             break
@@ -78,7 +85,7 @@ function least_interval_test() {
         do
             let i=i+test_step
             compile_and_run $i
-            python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f least_interval -a ${i},${timing_cost_file},${least_interval_file}
+            python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f least_interval -a ${i},${cost_measure_file},${least_interval_file}
             passed=`cat ${least_interval_file}`
             if [[ ${passed} == "no" ]]; then
                 let start=i
@@ -118,7 +125,7 @@ function variation_resolution() {
             do
                 compile_and_run $i
             done
-            python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f variation_resolution -a ${k},${least_round},${timing_cost_file},${resolution_file}
+            python algo.py -s 1,1 -e 1,2 -i ${output_dir} -b ${backend} -f variation_resolution -a ${k},${least_round},${cost_measure_file},${resolution_file}
             passed=`cat ${resolution_file}`
             if [[ ${passed} == "no" ]]; then
                 let left=k
@@ -134,7 +141,7 @@ function variation_resolution() {
 # step-0: fix the frequency
 sudo cpupower frequency-set -g performance -u 2.6G -d 2.6G
 # step-1: timing instruction cost test
-timing_cost_test
+cost_measure_test
 # step-2: least measurable interval
 least_interval_test
 # step-3: variation resolution
